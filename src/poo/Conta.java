bb2ebab17e354eec8764c73760fd558546981790
Mateus Dias <mateusdias.her@gmail.com>
2022-02-21 00:59:40 -0300

    Adição de novos arquivos

--- src/poo/Conta.java
+++ src/poo/Conta.java
@@ -7,5 +7,15 @@ public class Conta {
    void exibeSaldo(){
        System.out.println(cliente + " seu saldo é: R$ "+ saldo);
    }
+   void saca (double valor){
+       saldo -= valor;
+   }
+   void depositar (double valor){
+       saldo += valor;
+   }
+   void transferePara(Conta destino, double valor){
+       this.saca(valor);
+       destino.depositar(valor);
+   }
 }
 
--- src/poo/ContaTest.java
+++ src/poo/ContaTest.java
@@ -6,6 +6,15 @@ public class ContaTest {
         conta.cliente = "Mateus";
         conta.saldo = 10_000;
         conta.exibeSaldo();
+
+        Conta destino = new Conta();
+        destino.cliente = "Tairo";
+        destino.saldo = 8_000.00;
+        destino.exibeSaldo();
+
+        conta.transferePara(destino, 1550);
+        conta.exibeSaldo();
+        destino.exibeSaldo();
     }
 
 }
